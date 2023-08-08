ddd04c79a760a1ea8de23c103b48d881d55777cd
洮羱芝闇 <shun_@outlook.com>
2023-08-08 10:54:16 +0800

    实现了双向绑定

--- n3-template/src/app/app.component.ts
+++ n3-template/src/app/app.component.ts
@@ -10,4 +10,12 @@ export class AppComponent {
   msg2 = `<h1>Hello World</h1>`;
   img = "https://avatars.githubusercontent.com/u/83948628?v=4";
   isDisabled = true;
+  list = ['a', 'b', 'c'];
+  isShow = false;
+  num = 3;
+  put = '';
+
+  giao(){
+    alert('Giao');
+  }
 }
